c90553981cdb63d4966c8fcc5eeae6b38f9db045
yunkwangyou <>
2022-03-15 16:54:28 +0900

    Protocol Seperation
    Protocol Seperation(HTTP) - json파일만 분리하기 위함

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
-# 1. Simple Usage
+import pandas as pd
+import json
 
-from json_excel_converter import Converter
-from json_excel_converter.xlsx import Writer
+file_path = "./trace_log.txt"
 
-data = [
-    {'a': [1], 'b': 'hello'},
-    {'a': [1, 2, 3], 'b': 'world'}
-]
+data = ''
+res = ''
+with open(file_path, 'r') as file:
+    while 1 :
+        b = file.readline()
+        if b == '' : # 마지막 줄이면 프로그램 종료
+            break
+        elif b == '   PROTOCOL             = HTTP\n' : # 프로토콜이 HTTP면
+            while 1 :
+                c = file.readline()
+                if c == 'END\n' : # END 가 나올때까지 조회해서 결과값에 붙여줌
+                    break
+                res+=c
 
-conv = Converter()
-conv.convert(data, Writer(file='/tmp/test.xlsx'))
 
-
-"""
-# 2. Streaming Usage with Restart
-
-from json_excel_converter import Converter, LinearizationError
-from json_excel_converter.csv import Writer
-
-conv = Converter()
-writer = Writer(file='/tmp/test.csv')
-while True:
-    try:
-        data = get_streaming_data()     # custom function to get iterator of data
-        conv.convert_streaming(data, writer)
-        break
-    except LinearizationError:
-        pass
-"""
-
-"""
-
-"""
+print(res.index('{'))
+print(res)
